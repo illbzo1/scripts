89aa8676c3f5181f7a31349d4289c5588b26a38e
Tyler Moore <tylerlmoore@gmail.com>
2017-01-26 20:35:19 -0500

    Simplified script, fixed some bugs, began accounting for a large first page

--- book.rb
+++ book.rb
@@ -11,17 +11,17 @@ puts "How many days will the book take you to read?"
 days = gets.chomp.to_i
 days = days.to_i
 
-daily_reading = (total_pages / days).to_i
+daily_pages = (total_pages / days).to_i
 
-puts "You'll have to read #{daily_reading} pages per day to finish this book in #{days} days."
+puts "You'll have to read #{daily_pages} pages per day to finish this book in #{days} days."
 
 puts "Reading Schedule:"
 
-scheduled_reading = daily_reading
+scheduled_reading = (first_page + daily_pages)
 
-while scheduled_reading < total_pages and (total_pages - scheduled_reading) > daily_reading do
+while scheduled_reading < total_pages do
   puts "Read to page #{scheduled_reading}"
-  scheduled_reading += daily_reading
+  scheduled_reading += daily_pages
 end
 
 puts "Finish the book."
