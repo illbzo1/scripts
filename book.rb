b95077e7afddd628be7162fa41c5c97b660c5e00
Tyler Moore <tylerlmoore@gmail.com>
2018-01-24 22:06:05 -0500

    Add protection for short books

--- book.rb
+++ book.rb
@@ -6,22 +6,24 @@ last_page = gets.chomp.to_i
 
 total_pages = last_page - first_page
 
-puts "How many days will the book take you to read?"
+if total_pages < 8
+  puts "Just read it in one day, dummy!"
+else
+  puts "How many days will the book take you to read?"
 
-days = gets.chomp.to_i
-days = days.to_i
+  days = gets.chomp.to_i
+  days = days.to_i
 
-daily_pages = (total_pages / days).to_i
+  daily_pages = (total_pages / days).to_i
 
-puts "You'll have to read #{daily_pages} pages per day to finish this book in #{days} days."
+  puts "Here's your reading schedule:"
 
-puts "Reading Schedule:"
+  scheduled_reading = (first_page + daily_pages)
 
-scheduled_reading = (first_page + daily_pages)
-
-while scheduled_reading < total_pages do
-  puts "Read to page #{scheduled_reading}"
-  scheduled_reading += daily_pages
+  while scheduled_reading < total_pages do
+    puts "Read to page #{scheduled_reading}"
+    scheduled_reading += daily_pages
+  end
+  puts "Finish the book."
 end
 
-puts "Finish the book."
